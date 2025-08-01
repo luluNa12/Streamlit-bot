d76cc4248a22bc58560254dc8892cf6d595d0829
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2025-08-01 12:28:46 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,17 +1,17 @@
 
 #this is fourth
-import streamlit as st
+# import streamlit as st
 
-st.title("Echo Bot")
+# st.title("Echo Bot")
 
 # Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+# if "messages" not in st.session_state:
+#     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+# # Display chat messages from history on app rerun
+# for message in st.session_state.messages:
+#     with st.chat_message(message["role"]):
+#         st.markdown(message["content"])
 
 #this is fifth
 # React to user input
