72327c490e664c37bab52834242147b82a74a4dd
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2025-08-01 12:04:01 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -31,7 +31,7 @@ if prompt := st.chat_input("What is up?"):
     
     # Add assistant response to chat history
    # st.session_state.messages.append({"role": "assistant", "content": response})
- st.session_state.messages.append({"role": "assistant", "content": result})
+     st.session_state.messages.append({"role": "assistant", "content": result})
     
 
 
