918d3ff3360a22a23079b31978a27ea9cbcf9339
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2025-08-01 12:04:51 -0700

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -26,12 +26,12 @@ if prompt := st.chat_input("What is up?"):
     # Display assistant response in chat message container
     
     with st.chat_message("assistant"):
-      #  st.markdown(response)
-        st.markdown(result)
+          st.markdown(response)
+        
     
     # Add assistant response to chat history
-   # st.session_state.messages.append({"role": "assistant", "content": response})
-     st.session_state.messages.append({"role": "assistant", "content": result})
+      st.session_state.messages.append({"role": "assistant", "content": response})
+    
     
 
 
