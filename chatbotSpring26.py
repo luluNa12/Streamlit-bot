2a8eef1fb2a6e45fa095267f52b0f08f0e8b946d
luluNa12 <121836520+luluNa12@users.noreply.github.com>
2026-01-02 18:05:56 -0800

    Implement response generator for chat responses
    Added a response generator function to simulate streamed responses in the chat.

--- chatbotSpring26.py
+++ chatbotSpring26.py
@@ -4,6 +4,19 @@ import time
 
 st.title("Simple chat")
 
+# Streamed response emulator
+def response_generator():
+    response = random.choice(
+        [
+            "Hello there! How can I assist you today?",
+            "Hi, human! Is there anything I can help you with?",
+            "Do you need help?",
+        ]
+    )
+    for word in response.split():
+        yield word + " "
+        time.sleep(0.05)
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
